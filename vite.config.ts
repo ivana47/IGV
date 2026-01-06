c561a892e035540fc17b2e4cb6d020b5fb6ea8f7
Ivana <ivana.pavic@fet.ba>
2026-01-06 23:49:18 +0100

    trying to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/IGV/"
+  base: "/IGV/",
+  build: {
+    rollupOptions: {
+      output: {
+        // ovo isključuje hash u imenima fajlova
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
 })
